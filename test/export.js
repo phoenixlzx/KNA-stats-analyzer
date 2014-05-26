5836f9e2371b0d1f1e4779e9b5ac5e52ae56f91a
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-05-26 12:10:22 +0800

    fixed title index

---
+++ test/export.js
@@ -0,0 +1,19 @@
+/**
+ * 下午12:05
+ * Phoenix Nemo <i at phoenixlzx dot com>
+ * License MIT | http://opensource.org/licenses/MIT
+ */
+
+
+var query = {
+    title: '魔法高校的劣等生'
+};
+
+var fs = require('fs');
+
+var MongoClient = require('mongodb').MongoClient;
+
+var config = require('./config');
+
+
+
