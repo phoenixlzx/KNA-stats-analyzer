2a6b6c82ca37f87554f31d614bc5a5d3b616b1fe
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-05-26 12:27:12 +0800

    use async instead

--- test/export.js
+++ test/export.js
@@ -13,6 +13,8 @@ var fs = require('fs');
 
 var MongoClient = require('mongodb').MongoClient;
 
+var async = require('async');
+
 var config = require('../config');
 
 var data = '# 数据来源\t字幕组\t发布日期\t标题\t种子\t下载\t完成\t抓取时间\n';
@@ -32,15 +34,19 @@ MongoClient.connect(config.mongodb, { db: { native_parser: true, w : 1 } }, func
         taskTime: 1
     }).toArray(function(err, docs) {
         if (err) throw err;
-        docs.forEach(function(doc) {
+
+        async.eachSeries(docs, function(doc, cb) {
             data += doc.source + '\t' + doc.team + '\t' + doc.publishDate + '\t' +
                 doc.torrents + '\t' + doc.downloads + '\t' + doc.finish + '\t' + doc.taskTime + '\n';
+            cb();
+        }, function() {
+            fs.writeFile('./exports_data.txt', data, function (err) {
+                if (err) throw err;
+                console.log('Data exported.');
+                process.exit(0);
+            });
         });
 
-        fs.writeFile('./exports_data.txt', data, function (err) {
-            if (err) throw err;
-            console.log('Data exported.');
-        });
     });
 
 });
